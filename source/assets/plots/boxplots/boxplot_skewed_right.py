d0381f358a07ab1abc9bda1318d66058b4124455
bw-math <gmoore@bishopwalsh.org>
2023-09-02 08:35:32 -0400

    skewing the histograms

--- source/assets/plots/boxplots/boxplot_skewed_right.py
+++ source/assets/plots/boxplots/boxplot_skewed_right.py
@@ -45,7 +45,7 @@ fig, axs = plt.subplots(1, 2)
 # Generate Data
 # NOTE: You can add the contents of lists together with "+"
 data = ( 
-    [ 50*rand.random() for _ in range(3) ] + # generate some random F's, 0 - 49
+    [ 9*rand.random() + 40 for _ in range(2) ] + # generate some random F's, 0 - 49
     [ 9*rand.random() + 50 for _ in range(5) ] + # generate some random E's, 50 - 59 
     [ 9*rand.random() + 60 for _ in range(4) ] + # generate some random D's, 60 -69
     [ 9*rand.random() + 70 for _ in range(4) ] + # generate some random C's, 70- 79
